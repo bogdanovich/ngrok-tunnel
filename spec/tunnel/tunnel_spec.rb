2ae68b46de56aab91696a1f613b9c8c084119b3a
Anton Bogdanovich <27bogdanovich@gmail.com>
2016-02-13 10:29:05 -0800

    Fix spec descriptions

--- spec/tunnel/tunnel_spec.rb
+++ spec/tunnel/tunnel_spec.rb
@@ -2,64 +2,61 @@ describe Ngrok::Tunnel do
 
   describe "Before start" do
 
-    it "should not be running" do
+    it "is not running" do
       expect(Ngrok::Tunnel.running?).to be false
     end
 
-    it "should be stopped" do
+    it "is stopped" do
       expect(Ngrok::Tunnel.stopped?).to be true
     end
 
-    it "should have status = :stopped" do
+    it "has :stopped status" do
       expect(Ngrok::Tunnel.status).to eq :stopped
     end
 
   end
 
-  describe "Run process" do
+  describe "After start" do
 
-    before(:all) do
-      Ngrok::Tunnel.start
-    end
+    before(:all) { Ngrok::Tunnel.start }
+    after(:all)  { Ngrok::Tunnel.stop }
 
-    after(:all) { Ngrok::Tunnel.stop }
-
-    it "should be running" do
+    it "is running" do
       expect(Ngrok::Tunnel.running?).to be true
     end
 
-    it "should not be stopped" do
+    it "is not stopped" do
       expect(Ngrok::Tunnel.stopped?).to be false
     end
 
-    it "should have status = :running" do
+    it "has :running status" do
       expect(Ngrok::Tunnel.status).to eq :running
     end
 
-    it "should still support port" do
+    it "has correct port property" do
       expect(Ngrok::Tunnel.port).to eq(3001)
     end
 
-    it "should match local_addr" do
+    it "has correct addr property" do
       expect(Ngrok::Tunnel.addr).to eq(3001)
     end
 
-    it "should have valid ngrok_url" do
+    it "has valid ngrok_url" do
       expect(Ngrok::Tunnel.ngrok_url).to be =~ /http:\/\/.*ngrok\.io$/
     end
 
-    it "should have valid ngrok_url_https" do
+    it "has valid ngrok_url_https" do
       expect(Ngrok::Tunnel.ngrok_url_https).to be =~ /https:\/\/.*ngrok\.io$/
     end
 
-    it "should have pid > 0" do
+    it "has correct pid property" do
       expect(Ngrok::Tunnel.pid).to be > 0
     end
 
   end
 
   describe "Custom log file" do
-    it "should be able to use custom log file" do
+    it "uses custom log file" do
       Ngrok::Tunnel.start(log: 'test.log')
       expect(Ngrok::Tunnel.running?).to eq true
       expect(Ngrok::Tunnel.log.path).to eq 'test.log'
@@ -69,41 +66,41 @@ describe Ngrok::Tunnel do
   end
 
   describe "Custom subdomain" do
-    it "should fail without authtoken" do
+    it "fails without authtoken" do
       expect {Ngrok::Tunnel.start(subdomain: 'test-subdomain')}.to raise_error Ngrok::Error
     end
 
-    it "should fail with incorrect authtoken" do
+    it "fails with incorrect authtoken" do
       expect {Ngrok::Tunnel.start(subdomain: 'test-subdomain', authtoken: 'incorrect_token')}.to raise_error Ngrok::Error
     end
   end
 
   describe "Custom hostname" do
-    it "should fail without authtoken" do
+    it "fails without authtoken" do
       expect {Ngrok::Tunnel.start(hostname: 'example.com')}.to raise_error Ngrok::Error
     end
 
-    it "should fail with incorrect authtoken" do
+    it "fails with incorrect authtoken" do
       expect {Ngrok::Tunnel.start(hostname: 'example.com', authtoken: 'incorrect_token')}.to raise_error Ngrok::Error
     end
   end
 
   describe "Custom addr" do
-    it "should map port param to addr" do
+    it "maps port param to addr" do
       port = 10010
       Ngrok::Tunnel.start(port: port)
       expect(Ngrok::Tunnel.addr).to eq port
       Ngrok::Tunnel.stop
     end
 
-    it "should return just the port when the address contains a host" do
+    it "returns just the port when the address contains a host" do
       addr = '192.168.0.5:10010'
       Ngrok::Tunnel.start(addr: addr)
       expect(Ngrok::Tunnel.port).to eq 10010
       Ngrok::Tunnel.stop
     end
 
-    it "should support remote addresses" do
+    it "supports remote addresses" do
       addr = '192.168.0.5:10010'
       Ngrok::Tunnel.start(addr: addr)
       expect(Ngrok::Tunnel.addr).to eq addr
@@ -112,13 +109,13 @@ describe Ngrok::Tunnel do
   end
 
   describe "Custom parameters provided" do
-    it "should not include the -inspect parameter when it is not provided" do
+    it "doesn't include the -inspect parameter when it is not provided" do
       Ngrok::Tunnel.start()
       expect(Ngrok::Tunnel.send(:ngrok_exec_params).include? "-inspect=").to be false
       Ngrok::Tunnel.stop
     end
 
-    it "should include the -inspect parameter with the correct value when it is provided" do
+    it "includes the -inspect parameter with the correct value when it is provided" do
       Ngrok::Tunnel.start(inspect: true)
       expect(Ngrok::Tunnel.send(:ngrok_exec_params).include? "-inspect=true").to be true
       Ngrok::Tunnel.stop
